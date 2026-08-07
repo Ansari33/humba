eef1533e59637499257cf6d4b21bc605b52b0113
Ansari33 <Putraansari05@gmail.com>
2026-08-07 20:27:58 +0800

    fix slider

---
+++ check_html.py
@@ -0,0 +1,17 @@
+﻿from html.parser import HTMLParser
+class Checker(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.errors=[]
+    def error(self, message):
+        self.errors.append(message)
+with open('index.html','r',encoding='utf8') as f:
+    data=f.read()
+parser=Checker()
+parser.feed(data)
+parser.close()
+print('parse-ok')
+if parser.errors:
+    print('errors:')
+    for e in parser.errors:
+        print(e)
